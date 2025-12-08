e7f480c5a55f528e277a8b2142452e73360a2855
Caio Freitas Ferreira de Lima <102872237+caiofflima@users.noreply.github.com>
2025-12-08 18:59:25 -0300

    a1

--- src/app/shared/services/comum/integracao-correios.service.ts
+++ src/app/shared/services/comum/integracao-correios.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {CrudHttpClientService} from '../../../../app/arquitetura/shared/services/crud-http-client.service';
 import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 import {MessageService} from "../../components/messages/message.service";
 import { EnderecoCorreios } from '../../../../app/shared/models/comum/endereco-correios';
 import { Localidade } from '../../../../app/shared/models/localidate';
@@ -18,6 +19,10 @@ export class IntegracaoCorreiosService  extends CrudHttpClientService<EnderecoCo
     }
 
     getEnderecoByCEP(cep: string): Observable<EnderecoCorreios> {
-        return this.http.get<EnderecoCorreios>(`${this.url}/cep/${cep}`);
+        const url = `${this.url}/cep/${cep}`;
+        console.log('URL da requisição:', url);
+        return this.http.get<EnderecoCorreios>(url).pipe(
+            tap(response => console.log('Resposta completa da API:', response))
+        );
     }
 }
